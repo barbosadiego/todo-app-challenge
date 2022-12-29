2ff5cb26347c43e8b51cdfb5ac217a5f06e26142
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 11:05:35 -0300

    add tasks context to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { TaskContextProvider } from './contexts/taskContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <TaskContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TaskContextProvider>
   </React.StrictMode>,
 );
