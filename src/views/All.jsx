b0ae96cb1b265ef12084716ac5da006b8edc8858
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 11:33:47 -0300

    add render list in All.jsx

--- src/views/All.jsx
+++ src/views/All.jsx
@@ -1,15 +1,21 @@
 import React, { useContext } from 'react';
 import Input from '../components/Input';
+import Item from '../components/Item';
 import { TaskContext } from '../contexts/taskContext';
 import ShowPage from '../styles/ShowPage';
 
 const All = () => {
   const { initialTasks } = useContext(TaskContext);
   console.log(initialTasks);
+  // console.log(Date.now());
 
   return (
     <ShowPage>
       <Input />
+      {initialTasks.length &&
+        initialTasks.map((item) => (
+          <Item key={item.id} description={item.title} id={item.id} />
+        ))}
     </ShowPage>
   );
 };
