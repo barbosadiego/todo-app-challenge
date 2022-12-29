feac92cd0d170169caddc1f8e1015daf78125070
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 16:08:36 -0300

    add delete method in Button on Completed.jsx

--- src/views/Completed.jsx
+++ src/views/Completed.jsx
@@ -5,7 +5,7 @@ import { TaskContext } from '../contexts/taskContext';
 import ShowPage from '../styles/ShowPage';
 
 const Completed = () => {
-  const { tasks } = useContext(TaskContext);
+  const { tasks, setTasks } = useContext(TaskContext);
 
   return (
     <ShowPage>
@@ -13,17 +13,20 @@ const Completed = () => {
         tasks.map((item) => (
           <Item key={item.id} item={item} deleteBtn={true} />
         ))}
-      <Button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="20"
-          width="20"
-          fill="rgb(255,255,255)"
-        >
-          <path d="M6.5 17q-.625 0-1.062-.438Q5 16.125 5 15.5v-10H4V4h4V3h4v1h4v1.5h-1v10q0 .625-.438 1.062Q14.125 17 13.5 17Zm7-11.5h-7v10h7ZM8 14h1.5V7H8Zm2.5 0H12V7h-1.5Zm-4-8.5v10Z" />
-        </svg>
-        delete all
-      </Button>
+      {tasks.length > 0 && (
+        <Button onClick={() => setTasks([])}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            width="20"
+            fill="rgb(255,255,255)"
+          >
+            <path d="M6.5 17q-.625 0-1.062-.438Q5 16.125 5 15.5v-10H4V4h4V3h4v1h4v1.5h-1v10q0 .625-.438 1.062Q14.125 17 13.5 17Zm7-11.5h-7v10h7ZM8 14h1.5V7H8Zm2.5 0H12V7h-1.5Zm-4-8.5v10Z" />
+          </svg>
+          delete all
+        </Button>
+      )}
+      {tasks.length === 0 && <p>No items to show.</p>}
     </ShowPage>
   );
 };
