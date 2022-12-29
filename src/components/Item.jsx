b35bb464399c0d62fa231588b498d56b844caa1c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 15:49:54 -0300

    add new prop and style in Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { TaskContext } from '../contexts/taskContext';
+import deleteIcon from '../assets/delete.svg';
 
-const Item = ({ item }) => {
+const Item = ({ item, deleteBtn = false }) => {
   const { tasks, setTasks } = useContext(TaskContext);
 
   function handleClick(e) {
@@ -23,15 +24,22 @@ const Item = ({ item }) => {
 
   return (
     <StyledItem>
-      <input
-        type="checkbox"
-        name={item.id}
-        id={item.id}
-        onClick={(e) => handleClick(e)}
-        checked={item.completed}
-        readOnly
-      />
-      <label htmlFor={item.id}>{item.title}</label>
+      <div>
+        <input
+          type="checkbox"
+          name={item.id}
+          id={item.id}
+          onClick={(e) => handleClick(e)}
+          checked={item.completed}
+          readOnly
+        />
+        <label htmlFor={item.id}>{item.title}</label>
+      </div>
+      {deleteBtn && (
+        <Button>
+          <img src={deleteIcon} alt="delete Button" />
+        </Button>
+      )}
     </StyledItem>
   );
 };
@@ -41,8 +49,14 @@ export default Item;
 const StyledItem = styled.div`
   display: flex;
   align-items: center;
-  gap: 5px;
+  justify-content: space-between;
   margin-bottom: 25px;
+
+  & > div {
+    gap: 5px;
+    display: flex;
+    align-items: center;
+  }
 
   input[type='checkbox'] {
     width: 24px;
@@ -58,5 +72,17 @@ const StyledItem = styled.div`
     font-size: 18px;
     font-family: 'Montserrat', sans-serif;
     line-height: 22px;
+  }
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  width: 45px;
+  height: 45px;
+  background-color: transparent;
+  border: none;
+
+  img {
+    max-width: 100%;
   }
 `;
