008583e36e6ca6419c79ec3162ad66857c6a85fe
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 15:14:14 -0300

    update and add context in Active.jsx view

--- src/views/Active.jsx
+++ src/views/Active.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Input from '../components/Input';
+import Item from '../components/Item';
+import { TaskContext } from '../contexts/taskContext';
 import ShowPage from '../styles/ShowPage';
 
 const Active = () => {
-  return <ShowPage>Active</ShowPage>;
+  const { tasks } = useContext(TaskContext);
+
+  return (
+    <ShowPage>
+      <Input />
+      {tasks.length > 0 &&
+        tasks.map((item) => <Item key={item.id} item={item} />)}
+    </ShowPage>
+  );
 };
 
 export default Active;
