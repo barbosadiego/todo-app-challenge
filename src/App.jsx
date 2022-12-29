07fd57d4bc04ab46e36201882a6fd9ae17c77b58
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 09:26:55 -0300

    add Title component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Title>#todo</Title>
+    </>
+  );
 };
 
 export default App;
+
+const Title = styled.h1`
+  font-family: 'Raleway', sans-serif;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+`;
