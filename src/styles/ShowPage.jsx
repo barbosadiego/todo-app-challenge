2332be2b623685a34247afee2be7449791d80f99
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 10:30:08 -0300

    create page animation ShowPage.jsx

---
+++ src/styles/ShowPage.jsx
@@ -0,0 +1,24 @@
+import styled, { keyframes } from 'styled-components';
+import React from 'react';
+
+const ShowPage = ({ children }) => {
+  return <StyledShowPage>{children}</StyledShowPage>;
+};
+
+export default ShowPage;
+
+const showLeft = keyframes`
+  from {
+    transform: translateX(-30px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+`;
+
+const StyledShowPage = styled.section`
+  animation: ${showLeft} 0.3s forwards;
+`;
