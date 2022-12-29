4bf1e912008daaaf2e77045aa6acf314f1a669aa
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-29 09:36:32 -0300

    add routes and links in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Routes, Route, Link } from 'react-router-dom';
+import All from './views/All';
+import Active from './views/Active';
+import Completed from './views/Completed';
 
 const App = () => {
   return (
     <>
       <Title>#todo</Title>
+      <div>
+        <Link to="/">All</Link>
+        <Link to="/active">Active</Link>
+        <Link to="/completed">Completed</Link>
+      </div>
+      <hr />
+      <Routes>
+        <Route path="/" element={<All />} />
+        <Route path="/active" element={<Active />} />
+        <Route path="/completed" element={<Completed />} />
+      </Routes>
     </>
   );
 };
