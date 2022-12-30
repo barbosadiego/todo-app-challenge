d2ac471fec09f80eb4ae06879208896af2e76243
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-30 11:14:00 -0300

    add local storage method in context

--- src/contexts/taskContext.js
+++ src/contexts/taskContext.js
@@ -1,12 +1,24 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const TaskContext = createContext();
 
 export const TaskContextProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
 
+  function saveData(data) {
+    localStorage.setItem('todos', JSON.stringify(data));
+  }
+
+  function getData() {
+    return JSON.parse(localStorage.getItem('todos'));
+  }
+
+  useEffect(() => {
+    setTasks(getData());
+  }, []);
+
   return (
-    <TaskContext.Provider value={{ tasks, setTasks }}>
+    <TaskContext.Provider value={{ tasks, setTasks, saveData }}>
       {children}
     </TaskContext.Provider>
   );
